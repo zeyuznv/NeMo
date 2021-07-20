52726ec08f32f8454a4c70b6b65ea72c7f530743
Anton Peganov <apeganov@nvidia.com>
2021-07-20 20:53:43 +0300

    Add script for IWSLT audio -> NeMo format
    Signed-off-by: Anton Peganov <apeganov@nvidia.com>

---
+++ scripts/dataset_processing/prepare_iwslt_audio_data.py
@@ -0,0 +1,98 @@
+import argparse
+import contextlib
+import json
+import re
+from pathlib import Path
+
+import wave
+from bs4 import BeautifulSoup
+
+
+TALK_ID_COMPILED_PATTERN = re.compile(r"[1-9][0-9]*(?=\.wav)$")
+DOC_COMPILED_PATTERN = re.compile(r'<doc docid="([1][0-9]*)"[^>]*>')
+REMOVE_NOT_ALPHABETICAL_PATTERN = re.compile(r'[^a-z ]')
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--audio-dir",
+        "-a",
+        required=True,
+        type=Path,
+    )
+    parser.add_argument(
+        "--src-text",
+        "-t",
+        required=True,
+        type=Path,
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        required=True,
+        type=Path,
+    )
+    args = parser.parse_args()
+    args.audio_dir = args.audio_dir.expanduser()
+    if not args.audio_dir.is_dir():
+        raise ValueError("`audio_dir` parameter has to be a path to directory.")
+    args.src_text = args.src_text.expnaduser()
+    if not args.src_text.is_file():
+        raise ValueError("`src_text` parameter has to be a path to a file.")
+    args.src_text = args.output.expnaduser()
+    if not args.output.is_file():
+        raise ValueError("`output` parameter has to be a path to a file.")
+    return args
+
+
+def get_wav_files(audio_dir):
+    result = {}
+    for elem in audio_dir.iterdir():
+        m = TALK_ID_COMPILED_PATTERN.search(str(elem))
+        if elem.is_file() and m is not None:
+            result[m.group(0)] = elem
+    return result
+
+
+def get_talk_id_to_text(src_text):
+    with src_text.open() as f:
+        text = f.read()
+    soup = BeautifulSoup(text)
+    docs = soup.findAll("doc")
+    result = {
+        doc["docid"]:
+            REMOVE_NOT_ALPHABETICAL_PATTERN.sub('', ' '.join([elem.text for elem in doc.findAll("seg")]).lower())
+        for doc in docs}
+    return result
+
+
+def get_wav_duration(filepath):
+    with contextlib.closing(wave.open(filepath, 'r')) as f:
+        frames = f.getnframes()
+        rate = f.getframerate()
+    return frames / float(rate)
+
+
+def main():
+    args = get_args()
+    talk_id_to_wav_file = get_wav_files(args.audio_dir)
+    talk_id_to_text = get_talk_id_to_text(args.src_text)
+    manifest = ""
+    if len(talk_id_to_text) != len(talk_id_to_wav_file):
+        raise ValueError(
+            f"Number of documents described in the XML file {args.src_text} is not equal to the number "
+            f"of wav files with talk ids found in the directory {args.audio_dir}.\nNumber of documents in the XML "
+            f"file: {len(talk_id_to_text)}, number of wav files: {len(talk_id_to_wav_file)}.\n.wav files: "
+            f"{talk_id_to_wav_file}")
+    for talk_id, text in talk_id_to_text.items():
+        if manifest:
+            manifest += '\n'
+        filepath = talk_id_to_wav_file[talk_id]
+        manifest += json.dumps({"audio_filepath": str(filepath), "duration": get_wav_duration(filepath), "text": text})
+    with args.output.open() as f:
+        f.write(manifest)
+
+
+if __name__ == "__main__":
+    main()
