0a712f54b42a438b0fdc93efe5005a95842b07a0
PeganovAnton <peganoff2@mail.ru>
2021-07-21 18:54:49 +0300

    fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/create_de_ground_truth.sh
+++ examples/speech_translation/create_de_ground_truth.sh
@@ -3,4 +3,3 @@ work_dir=~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019
 python prepare_iwslt_text_for_translation.py -a "${work_dir}/manifest.json" \
     -t "${work_dir}/IWSLT.TED.tst2019.en-de.de.xml" \
     -o "${work_dir}/iwslt_de_text.txt"
-done
