b387ba981efd780f7cc179d6a53292dfbb6c0463
PeganovAnton <peganoff2@mail.ru>
2021-07-21 08:07:38 -0700

    fix minor bugs
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/compute_all_wers.sh
+++ examples/speech_translation/compute_all_wers.sh
@@ -1,6 +1,9 @@
+> "${output}"
 for f in "${pred_dir}"/*; do
   if [ "${f: -9}" == ".manifest" ]; then
-    wer=$(python wer_between_2_manifests.py "${f}" "${ground_truth}")
-    echo "${$(basename "${f}")%.*} ${wer}" >> "${output}"
+    wer="$(python wer_between_2_manifests.py "${f}" "${ground_truth}")"
+    fn="$(basename "${f}")"
+    model_name="${fn%.*}"
+    echo "${model_name} ${wer}" | tee -a "${output}"
   fi
 done
