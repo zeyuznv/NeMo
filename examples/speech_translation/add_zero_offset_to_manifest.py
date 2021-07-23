7bb99bda8336f08f5e9673b12183dcff78fbf18f
PeganovAnton <peganoff2@mail.ru>
2021-07-23 16:29:35 +0300

    Add script for adding zeroth offset
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/speech_translation/add_zero_offset_to_manifest.py
@@ -0,0 +1,28 @@
+import argparse
+import json
+from pathlib import Path
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", "-i", required=True, type=Path)
+    parser.add_argument("--output", "-o", required=True, type=Path)
+    args = parser.parse_args()
+    args.input = args.input.expanduser()
+    args.output = args.output.expanduser()
+    return args
+
+
+def main():
+    args = get_args()
+    with args.input.open() as in_f, args.output.open() as out_f:
+        for i, line in enumerate(in_f):
+            if i > 0:
+                out_f.write('\n')
+            data = json.loads(line)
+            data["offset"] = 0.0
+            data.write(json.dumps(data))
+
+
+if __name__ == "__main__":
+    main()
