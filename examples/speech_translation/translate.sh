bc2a5091c2a6972bb1d777406d1d2e148e65db4d
PeganovAnton <peganoff2@mail.ru>
2021-07-28 13:24:29 +0300

    Update translate all bash script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/translate.sh
+++ examples/speech_translation/translate.sh
@@ -1,59 +1,81 @@
 set -e -x
 
-good_transcript_models=(
-  stt_en_citrinet_1024_gamma_0_25
+transcript_models_not_only_segmented_data=(
+  QuartzNet15x5Base-En
+  stt_en_jasper10x5dr
+  stt_en_citrinet_256
+  stt_en_citrinet_512
   stt_en_citrinet_1024
+  stt_en_citrinet_256_gamma_0_25
+  stt_en_citrinet_512_gamma_0_25
+  stt_en_citrinet_1024_gamma_0_25
+  CitriNet-1024-8x-Stride-Gamma-0.25.nemo
+  Citrinet-1024-SPE-Unigram-1024-Jarvis-ASRSet-2.0_no_weight_decay_e250-averaged.nemo
 )
-#  stt_en_citrinet_256_gamma_0_25
-#  stt_en_citrinet_256
-#  stt_en_citrinet_512_gamma_0_25
-#  stt_en_citrinet_512
-#  stt_en_jasper10x5dr
-#)
+
+transcript_models_only_segmented_data=(
+  stt_en_conformer_ctc_small
+  stt_en_conformer_ctc_medium
+  stt_en_conformer_ctc_large
+  stt_en_conformer_ctc_small_ls
+  stt_en_conformer_ctc_medium_ls
+  stt_en_conformer_ctc_large_ls
+  sel_jarvisasrset_d512_adamwlr2_wd0_aug10x0.05_sp128_500e-last.nemo
+)
 
 work_dir=~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019
-translated_dir=translated_transcripts
-long_segments_result="${work_dir}/${translated_dir}/long_segments"
-one_sentence_segments_result="${work_dir}/${translated_dir}/one_sentence_segments"
-punc_transcripts="${work_dir}/punc_transcripts"
+translated_dirs=( translated_transcripts_segmented translated_transcripts_not_segmented )
+
+punc_transcripts=( "${work_dir}/punc_transcripts_segmented_input" "${work_dir}/transcript_models_only_segmented_data" )
 translation_checkpoints=( ~/checkpoints/wmt21_en_de_backtranslated_24x6_averaged.nemo )
 translation_ngc_models=( nmt_en_de_transformer12x2 )
 mkdir -p "${long_segments_result}"
 mkdir -p "${one_sentence_segments_result}"
 
-for ckpt in "${translation_checkpoints[@]}"; do
-  model_name=$(basename "${ckpt}")
-  save_dir="${long_segments_result}/${model_name}"
-  mkdir -p "${save_dir}"
-  for m in "${good_transcript_models[@]}"; do
-    python translate_iwslt.py -p "${ckpt}" \
-      -i "${punc_transcripts}/${m}.txt" \
-      -o "${save_dir}/${m}.txt"
+for i in {0..1}; do
+  if [ "${i}" -eq 0 ]; then
+    good_transcript_models=("${transcript_models_not_only_segmented_data[@]}" "${transcript_models_only_segmented_data[@]}")
+  else
+    good_transcript_models=("${transcript_models_not_only_segmented_data[@]}")
+  fi
+  translated_dir="${translated_dir[i]}"
+  punc_transcripts_dir="${punc_transcripts[i]}"
+  long_segments_result="${work_dir}/${translated_dir}/long_segments"
+  one_sentence_segments_result="${work_dir}/${translated_dir}/one_sentence_segments"
+  for ckpt in "${translation_checkpoints[@]}"; do
+    model_name=$(basename "${ckpt}")
+    save_dir="${long_segments_result}/${model_name}"
+    mkdir -p "${save_dir}"
+    for m in "${good_transcript_models[@]}"; do
+      python translate_iwslt.py -p "${ckpt}" \
+        -i "${punc_transcripts_dir}/${m}.txt" \
+        -o "${save_dir}/${m}.txt"
+    done
+
+    mkdir -p "${one_sentence_segments_result}/${ckpt}"
+    for m in "${good_transcript_models[@]}"; do
+      python translate_iwslt.py -p "${ckpt}" \
+        -i "${punc_transcripts_dir}/${m}.txt" \
+        -o "${one_sentence_segments_result}/${ckpt}/${m}.txt" \
+        -s
+    done
   done
 
-  mkdir -p "${one_sentence_segments_result}/${ckpt}"
-  for m in "${good_transcript_models[@]}"; do
-    python translate_iwslt.py -p "${ckpt}" \
-      -i "${punc_transcripts}/${m}.txt" \
-      -o "${one_sentence_segments_result}/${ckpt}/${m}.txt" \
-      -s
-  done
-done
+  for ngc_model in "${translation_ngc_models[@]}"; do
+    mkdir -p "${long_segments_result}/${ngc_model}"
+    for m in "${good_transcript_models[@]}"; do
+      python translate_iwslt.py -m "${ngc_model}" \
+        -i "${punc_transcripts_dir}/${m}.txt" \
+        -o "${long_segments_result}/${ngc_model}/${m}.txt"
+    done
 
-for ngc_model in "${translation_ngc_models[@]}"; do
-  mkdir -p "${long_segments_result}/${ngc_model}"
-  for m in "${good_transcript_models[@]}"; do
-    python translate_iwslt.py -m "${ngc_model}" \
-      -i "${punc_transcripts}/${m}.txt" \
-      -o "${long_segments_result}/${ngc_model}/${m}.txt"
-  done
-
-  mkdir -p "${one_sentence_segments_result}/${ngc_model}"
-  for m in "${good_transcript_models[@]}"; do
-    python translate_iwslt.py -m "${ngc_model}" \
-      -i "${punc_transcripts}/${m}.txt" \
-      -o "${one_sentence_segments_result}/${ngc_model}/${m}.txt" \
-      -s
+    mkdir -p "${one_sentence_segments_result}/${ngc_model}"
+    for m in "${good_transcript_models[@]}"; do
+      python translate_iwslt.py -m "${ngc_model}" \
+        -i "${punc_transcripts_dir}/${m}.txt" \
+        -o "${one_sentence_segments_result}/${ngc_model}/${m}.txt" \
+        -s
+    done
   done
 done
 
