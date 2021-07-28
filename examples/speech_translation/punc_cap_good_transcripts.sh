a9d49aeb2f21d78f9972c8c8de4117daee924095
PeganovAnton <peganoff2@mail.ru>
2021-07-28 10:44:52 +0300

    Update punctuation script for more models
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punc_cap_good_transcripts.sh
+++ examples/speech_translation/punc_cap_good_transcripts.sh
@@ -1,17 +1,42 @@
-good_transcript_models=(
-  stt_en_citrinet_1024_gamma_0_25
+pretrained_ngc_not_only_segmented_data=(
+  QuartzNet15x5Base-En
+  stt_en_jasper10x5dr
+  stt_en_citrinet_256
+  stt_en_citrinet_512
   stt_en_citrinet_1024
   stt_en_citrinet_256_gamma_0_25
-  stt_en_citrinet_256
   stt_en_citrinet_512_gamma_0_25
-  stt_en_citrinet_512
-  stt_en_jasper10x5dr
+  stt_en_citrinet_1024_gamma_0_25
+  CitriNet-1024-8x-Stride-Gamma-0.25.nemo
+  Citrinet-1024-SPE-Unigram-1024-Jarvis-ASRSet-2.0_no_weight_decay_e250-averaged.nemo
 )
+
+pretrained_ngc_only_segmented_data=(
+  stt_en_conformer_ctc_small
+  stt_en_conformer_ctc_medium
+  stt_en_conformer_ctc_large
+  stt_en_conformer_ctc_small_ls
+  stt_en_conformer_ctc_medium_ls
+  stt_en_conformer_ctc_large_ls
+  sel_jarvisasrset_d512_adamwlr2_wd0_aug10x0.05_sp128_500e-last.nemo
+)
+
 
 work_dir=~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019
 
-for m in "${good_transcript_models[@]}"; do
+segmented_transcripts_dir="${work_dir}/transcripts_not_segmented_input"
+unsegmented_transcripts_dir="${work_dir}/transcripts_segmented_input"
+
+punc_segmented_transcripts_dir="${work_dir}/punc_transcripts_not_segmented_input"
+punc_unsegmented_transcripts_dir="${work_dir}/punc_transcripts_segmented_input"
+
+for m in "${pretrained_ngc_not_only_segmented_data[@]}" "${pretrained_ngc_only_segmented_data[@]}"; do
   python punc_cap.py -a "${work_dir}/manifest.json" \
-    -p "${work_dir}/transcripts/${m}.manifest" \
-    -o "${work_dir}/punc_transcripts/${m}.txt"
+    -p "${segmented_transcripts_dir}/${m}.manifest" \
+    -o "${punc_segmented_transcripts_dir}/${m}.txt"
+done
+for m in "${pretrained_ngc_not_only_segmented_data[@]}"; do
+  python punc_cap.py -a "${work_dir}/manifest.json" \
+    -p "${unsegmented_transcripts_dir}/${m}.manifest" \
+    -o "${punc_unsegmented_transcripts_dir}/${m}.txt"
 done
--- examples/speech_translation/transcribe_with_different_models.sh
+++ examples/speech_translation/transcribe_with_different_models.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-pretrained_ngc_not_only_split_data=(
+pretrained_ngc_not_only_segmented_data=(
   QuartzNet15x5Base-En
   stt_en_jasper10x5dr
   stt_en_citrinet_256
@@ -9,13 +9,6 @@ pretrained_ngc_not_only_split_data=(
   stt_en_citrinet_512_gamma_0_25
   stt_en_citrinet_1024_gamma_0_25
 )
-#  stt_en_conformer_ctc_small
-#  stt_en_conformer_ctc_medium
-#  stt_en_conformer_ctc_large
-#  stt_en_conformer_ctc_small_ls
-#  stt_en_conformer_ctc_medium_ls
-#  stt_en_conformer_ctc_large_ls
-#)
 
 pretrained_ngc_only_segmented_data=(
   stt_en_conformer_ctc_small
@@ -38,7 +31,7 @@ nemo_asr_checkpoints_only_segmented_data=(
 
 checkpoint_dir=~/checkpoints
 workdir=~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019
-output_segmented="${workdir}/transcripts_for_segmented_input"
+output_segmented="${workdir}/transcripts_segmented_input"
 output_not_segmented="${workdir}/transcripts_not_segmented_input"
 audio_dir="${workdir}/wavs"
 echo "Creating output directories '${output_segmented}' and '${output_not_segmented}'"
@@ -67,7 +60,7 @@ for model_checkpoint in "${nemo_asr_checkpoints_not_only_segmented_data[@]}" "${
     fi
   done
 done
-for pretrained_name in "${pretrained_ngc_only_segmented_data[@]}" "${pretrained_ngc_not_only_split_data[@]}"; do
+for pretrained_name in "${pretrained_ngc_only_segmented_data[@]}" "${pretrained_ngc_not_only_segmented_data[@]}"; do
   mkdir -p "${split_transcripts}/${pretrained_name}"
   for f in "${split_data_path}"/*; do
     talk_id=$(basename "${f}")
@@ -82,32 +75,10 @@ for pretrained_name in "${pretrained_ngc_only_segmented_data[@]}" "${pretrained_
 done
 python join_split_wav_manifests.py -s "${split_transcripts}" -o "${output_segmented}" -n "${audio_dir}"
 
-for pretrained_name in "${pretrained_ngc_not_only_split_data[@]}"; do
+for pretrained_name in "${pretrained_ngc_not_only_segmented_data[@]}"; do
   python ~/NeMo/examples/asr/transcribe_speech.py pretrained_name="${pretrained_name}" \
     audio_dir="${audio_dir}" \
     output_filename="${output_not_segmented}/${pretrained_name}.manifest" \
     cuda=true \
     batch_size=1
 done
-
-
-
-
-
-#split_data_path="${audio_dir}/../split"
-#split_transcripts="${audio_dir}/../split_transcripts"
-#for f in "${split_data_path}"/*; do
-#  talk_id=$(basename "${f}")
-#  if [[ "${talk_id}" =~ ^[1-9][0-9]*$ ]]; then
-#    mkdir -p "${split_transcripts}/${talk_id}"
-#    for pretrained_name in "${pretrained_ngc_only_segmented_data[@]}"; do
-#      python ~/NeMo/examples/asr/transcribe_speech.py pretrained_name="${pretrained_name}" \
-#        audio_dir="${f}" \
-#        output_filename="${split_transcripts}/${talk_id}/${pretrained_name}.manifest" \
-#        cuda=true \
-#        batch_size=4
-#    done
-#  fi
-#done
-
-
