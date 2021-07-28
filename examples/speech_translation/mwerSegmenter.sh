af091d5818dae36644a6e54221bea5b42c3e55d3
PeganovAnton <peganoff2@mail.ru>
2021-07-28 18:56:38 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/mwerSegmenter.sh
+++ examples/speech_translation/mwerSegmenter.sh
@@ -7,13 +7,13 @@ result_dir="${workdir}/mwerSegmented"
 for tr in "${translations[@]}"; do
   for tr_segm in $(ls "${workdir}/${tr}"); do
     for tr_model in $(ls "${workdir}/${tr}/${tr_segm}"); do
-      for translated_text in $(ls "${workdir}/${tr}/${tr_segm}"); do
+      for translated_text in $(ls "${workdir}/${tr}/${tr_segm}/${tr_model}"); do
         ./segmentBasedOnMWER.sh "${workdir}/IWSLT.TED.tst2019.en-de.en.xml" \
           "${workdir}/IWSLT.TED.tst2019.en-de.de.xml" \
           "${workdir}/${tr}/${tr_segm}/${tr_model}/${translated_text}" \
           "${translated_text%.*}" \
           German \
-          "${translated_text%.*}.xml" \
+          "${result_dir}/${tr}/${tr_segm}/${tr_model}/${translated_text%.*}.xml" \
           no \
           1
       done
