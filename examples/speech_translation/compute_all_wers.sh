055ebb5d8e662b908a7d1292253f23404c3bf071
PeganovAnton <peganoff2@mail.ru>
2021-08-02 12:56:46 +0300

    Compute WER for individual audios
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/compute_all_wers.sh
+++ examples/speech_translation/compute_all_wers.sh
@@ -4,22 +4,25 @@ output_not_segmented=wer_scores_not_segmented.txt
 workdir=~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019
 pred_dir_segmented="${workdir}/transcripts_segmented_input"
 pred_dir_not_segmented="${workdir}/transcripts_not_segmented_input"
+wer_by_transcript_and_audio="${workdir}/wer_by_transcript_and_audio"
 ground_truth="${workdir}/manifest.json"
 > "${output_segmented}"
 > "${output_not_segmented}"
 for f in "${pred_dir_segmented}"/*; do
   if [ "${f: -9}" == ".manifest" ]; then
-    wer="$(python wer_between_2_manifests.py "${f}" "${ground_truth}")"
     fn="$(basename "${f}")"
     model_name="${fn%.*}"
+    wer="$(python wer_between_2_manifests.py "${f}" "${ground_truth}")" \
+      -o "${wer_by_transcript_and_audio}/segmented/${model_name}.json"
     echo "${model_name} ${wer}" | tee -a "${output_segmented}"
   fi
 done
 for f in "${pred_dir_not_segmented}"/*; do
   if [ "${f: -9}" == ".manifest" ]; then
-    wer="$(python wer_between_2_manifests.py "${f}" "${ground_truth}")"
     fn="$(basename "${f}")"
     model_name="${fn%.*}"
+    wer="$(python wer_between_2_manifests.py "${f}" "${ground_truth}")" \
+      -o "${wer_by_transcript_and_audio}/not_segmented/${model_name}.json"
     echo "${model_name} ${wer}" | tee -a "${output_not_segmented}"
   fi
 done
--- examples/speech_translation/wer_between_2_manifests.py
+++ examples/speech_translation/wer_between_2_manifests.py
@@ -1,17 +1,24 @@
 import argparse
 import json
+import re
 from pathlib import Path
 
 from nemo.collections.asr.metrics.wer import word_error_rate
+
+
+TALK_ID_COMPILED_PATTERN = re.compile(r"[1-9][0-9]*(?=\.wav$)")
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("hyp", type=Path)
     parser.add_argument("ref", type=Path)
+    parser.add_argument("--output", "-o", type=Path, help="Path to output JSON file")
     args = parser.parse_args()
     args.hyp = args.hyp.expanduser()
     args.ref = args.ref.expanduser()
+    if args.output is not None:
+        args.output = args.output.expanduser()
     return args
 
 
@@ -24,26 +31,56 @@ def read_texts_from_manifest(filepath, text_key):
     return result
 
 
-def align_texts(hyps, refs, hyp_file, ref_file):
+def align_texts(hyps, refs):
     aligned_h, aligned_r = [], []
     for k, v in hyps.items():
         aligned_h.append(v)
-        if k not in refs:
-            raise ValueError(
-                f"Talk with id {k} is not found in references. Hypothesis file: {hyp_file}. Reference file: {ref_file}")
         aligned_r.append(refs[k])
     return aligned_h, aligned_r
+
+
+def extract_talkid(filename):
+    m = TALK_ID_COMPILED_PATTERN.search(str(filename))
+    if m is None:
+        raise ValueError(f"Cannot extract talkid from file name {filename}")
+    else:
+        result = m.group(0)
+    return result
+
+
+def get_wer_by_talkid(hyps, refs):
+    result = {}
+    for k, v in hyps.items():
+        talkid = extract_talkid(k)
+        result[talkid] = word_error_rate([v], [refs[talkid]])
+    return result
+
+
+def check_input_data(hyps, refs, hyp_file, ref_file):
+    if len(hyps) != len(refs):
+        raise ValueError(f"Number of hypothesis texts {len(hyps)} in file {hyp_file} is not equal to number of "
+                         f"reference texts {len(refs)} in file {ref_file}")
+    if set(hyps.keys()) != set(refs.keys()):
+        raise ValueError(f"File with hypothesis texts contains hypotheses for .wav files "
+                         f"{set(hyps.keys()) - set(refs.keys())} which are not present in reference file {ref_file}.\n"
+                         f"File with reference texts contains references for .wav files "
+                         f"{set(refs.keys()) - set(hyps.keys())} which are not present in hypothesis file {hyp_file}")
 
 
 def main():
     args = get_args()
     hyps = read_texts_from_manifest(args.hyp, "pred_text")
     refs = read_texts_from_manifest(args.ref, "text")
-    if len(hyps) != len(refs):
-        raise ValueError(f"Number of hypothesis texts {len(hyps)} in file {args.hyp} is not equal to number of "
-                         f"reference texts {len(refs)} in file {args.ref}")
-    hyps, refs = align_texts(hyps, refs, args.hyp, args.ref)
-    print(word_error_rate(hyps, refs))
+    check_input_data(hyps, refs, args.hyp, args.ref)
+    aligned_hyps, aligned_refs = align_texts(hyps, refs)
+    wer = word_error_rate(aligned_hyps, aligned_refs)
+    print(wer)
+    if args.output is not None:
+        wer_by_talkid = get_wer_by_talkid(hyps, refs)
+        wer_by_talkid["all"] = wer
+        args.output.parent.mkdir(exist_ok=True, parents=True)
+        with args.output.open('w') as f:
+            json.dump(wer_by_talkid, f)
 
 
 if __name__ == "__main__":
