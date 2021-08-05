f23be6b554f8c6711515e531206bcd2e94474973
PeganovAnton <peganoff2@mail.ru>
2021-08-05 12:32:42 +0300

    Fix env activation
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/mwerSegmenter.sh
+++ examples/speech_translation/mwerSegmenter.sh
@@ -1,4 +1,18 @@
 set -e
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/lab/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/lab/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/lab/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/lab/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
 conda activate mwerSegmenter
 cd ~/mwerSegmenter/
 
