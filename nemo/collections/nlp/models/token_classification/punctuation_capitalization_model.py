d7fc035678811fc0337b051aaf663a816ac4d167
PeganovAnton <peganoff2@mail.ru>
2021-08-11 02:29:28 +0300

    Add debug print
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- nemo/collections/nlp/models/token_classification/punctuation_capitalization_model.py
+++ nemo/collections/nlp/models/token_classification/punctuation_capitalization_model.py
@@ -466,12 +466,14 @@ class PunctuationCapitalizationModel(NLPModel, Exportable):
                 for i, (q_i, start_word_id, b_punct_probs_i, b_capit_probs_i) in enumerate(
                         zip(query_ids, start_word_ids, b_punct_probs, b_capit_probs)):
                     print("q_i, b_punct_probs_i.shape, b_capit_probs_i.spape, acc_punct_probs[q_i].shape, acc_capit_probs[q_i].shape:", q_i, b_punct_probs_i.shape, b_capit_probs_i.shape, acc_punct_probs[q_i].shape if acc_punct_probs[q_i] is not None else None, acc_capit_probs[q_i].shape if acc_capit_probs[q_i] is not None else None)
+                    print("q_i, start_word_id:", q_i, start_word_id)
                     if acc_punct_probs[q_i] is None:
+                        print("acc is None")
                         acc_punct_probs[q_i] = b_punct_probs_i
                         assert acc_capit_probs[q_i] is None
                         acc_capit_probs[q_i] = b_capit_probs_i
                     else:
-                        print("q_i:", q_i)
+                        print("acc is not None")
                         all_punct_preds[q_i], acc_punct_probs[q_i] = \
                             self.move_from_accumulated_probabilities_to_token_predictions(
                                 all_punct_preds[q_i], acc_punct_probs[q_i], start_word_id - len(all_punct_preds[q_i]))
