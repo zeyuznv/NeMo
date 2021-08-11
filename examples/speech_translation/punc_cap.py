8d16d2e90b9dbf380bb68ed37198470d4c6922b5
PeganovAnton <peganoff2@mail.ru>
2021-08-11 04:24:14 +0300

    Reduce seq length
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punc_cap.py
+++ examples/speech_translation/punc_cap.py
@@ -79,10 +79,10 @@ def main():
     order = get_talk_id_order(args.manifest_to_align_with)
     texts_to_process = load_manifest_text(args.manifest_pred, "pred_text")
     texts = [texts_to_process[talk_id] for talk_id in order]
-    max_seq_len = 512
+    max_seq_len = 256
     processed = []
     processed_texts = model.add_punctuation_capitalization(
-        texts, batch_size=4, max_seq_length=max_seq_len, step=64, margin=32)
+        texts, batch_size=2, max_seq_length=max_seq_len, step=32, margin=32)
     for text in processed_texts:
         processed.append(DECIMAL.sub(decimal_repl, SPACE_DEDUP.sub(' ', text)))
         # processed.append(
